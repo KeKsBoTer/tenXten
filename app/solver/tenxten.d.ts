0ada20377bdce4e7fd69400b631af201478c4ceb
Simon Niedermayr <14186588+KeKsBoTer@users.noreply.github.com>
2021-08-23 15:20:40 +0200

    added generated js code for github pages

---
+++ app/solver/tenxten.d.ts
@@ -0,0 +1,30 @@
+declare namespace wasm_bindgen {
+	/* tslint:disable */
+	/* eslint-disable */
+	/**
+	* @param {any} js_object
+	* @returns {Array<any> | undefined}
+	*/
+	export function solve(js_object: any): Array<any> | undefined;
+	
+}
+
+declare type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+declare interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly solve: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+declare function wasm_bindgen (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
